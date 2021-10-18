d50d428f7b0e13cc0de0115ee8958328664cc6ac
mayacatt <92679665+mayacatt@users.noreply.github.com>
2021-10-17 22:37:26 -0400

    WOOHOO
    assigned nativity and glm for abundance proportions

--- Tree_Data/script/Abundance.R
+++ Tree_Data/script/Abundance.R
@@ -1,24 +1,61 @@
-Dawson.trees.total <- read.csv("output/Metadata.Dawson.csv")
+Dawson.trees.total <- read.csv("output/Dawson.Trees.With.Origin.csv")
 View(Dawson.trees.total)
 str(Dawson.trees.total)
 library(dplyr)
+length(unique(Dawson.trees.total$Site.Code))
 
 #SLL subset
 
 SLL.Species <- subset(Dawson.trees.total, Native_SLL=='Y', select=c(Site.Type, Site.Code))
+SLL.Species <- SLL.Species %>% 
+  add_count(Site.Code) %>%
+  distinct() %>%
+  rename("SLL.per.site"=n)
+SLL.Species$Region <- "SLL"
 View(SLL.Species)
-SLL.per.site <- count(SLL.Species, Site.Code, .drop=FALSE)
-View(Sll.per.site)
 
 #ETF subset
-ETF.Species <- subset(Dawson.trees.total, Native_ETF=='Y')
+ETF.Species <- subset(Dawson.trees.total, Native_ETF=='Y',select=c(Site.Type, Site.Code))
+ETF.Species <- ETF.Species %>%
+  add_count(Site.Code) %>%
+  distinct() %>%
+  rename("ETF.per.site"=n)
+ETF.Species$Region <- "ETF"
 View(ETF.Species)
-ETF.per.site <- count(ETF.Species, Site.Code)
-View(ETF.per.site)
 
+
+str(Dawson.trees.total)
 #Total trees per site
-Trees.per.site <- count(Dawson.trees.total, Site.Code)
+Trees.per.site <- Dawson.trees.total[,4:5] %>%
+  add_count(Site.Code) %>%
+  distinct() %>%
+  rename("total.per.site"=n)
 View(Trees.per.site)
 
 #Proportion per site
-SLL.per.site$proportion.SLL <- Sll.per.site/Trees.per.site
+
+SLL.proportions <- left_join(Trees.per.site, SLL.Species)
+SLL.proportions[is.na(SLL.proportions)] <- 0
+View(Native.proportions)
+str(Native.proportions)
+SLL.proportions$Proportion <- SLL.proportions$SLL.per.site/SLL.proportions$total.per.site
+
+ETF.proportions <- left_join(Trees.per.site, ETF.Species)
+ETF.proportions[is.na(ETF.proportions)] <-0
+ETF.proportions$Proportion <- ETF.proportions$ETF.per.site/ETF.proportions$total.per.site
+
+Dawson.proportions <- merge(SLL.proportions, ETF.proportions, all=TRUE)
+View(Dawson.proportions)
+
+install.packages("car")
+library(car)
+str(Dawson.proportions)
+glm.abundance <- glm(Proportion~ Site.Type+Region,weights=total.per.site, family=binomial, Dawson.proportions)
+anova(glm.abundance)
+Anova(glm.abundance)
+summary(glm.abundance)
+
+install.packages("emmeans")
+Yes
+library(emmeans)
+lsmeans(glm.abundance, pairwise~Site.Type|Region, adjust=Tukey)
---
+++ Tree_Data/script/Data_Cleaning.R
@@ -0,0 +1,64 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(dplyr)
+library(magrittr)
+
+meta.data <- read.csv("metadata.csv", na.strings="")
+View(meta.data)
+str(meta.data)
+
+#get rid of NA and recode 
+meta.data <- meta.data %>%
+  drop_na(Site.Code) %>%
+  distinct() %>%
+  mutate(Species.Code=recode(Species.Code, LOPER="LOPE")) %>%
+  mutate(Species.Code=recode(Species.Code, EU="LOPE")) %>%
+  mutate(Species.Code=recode(Species.Code, HOLONICERALONICERAEU="LOPE")) %>%
+  mutate(Species.Code=recode(Species.Code, ST="MAST")) %>%
+  mutate(Species.Code=recode(Species.Code, WE="WEFL")) %>%
+  mutate(Species_Common=recode(Species_Common, Wygeria ="Weigela Florida"))
+  
+meta.data$Cultivar.Code <- meta.data$Species.Code
+meta.data$Genus.Code <- substr(meta.data$Species.Code, 1,2)
+meta.data$Site.Type <- substr(meta.data$Site.Code, 1,1)
+  
+meta.data <- meta.data %>%
+  mutate(Species.Code = str_remove(Species.Code, "[XX]")) %>%
+  mutate(Species.Code = str_remove(Species.Code, "[X]")) %>%
+  mutate(Species.Code = substring(Species.Code, 1,4)) 
+         
+str(meta.data)
+View(meta.data)
+length(unique(meta.data$Cultivar.Code))
+#134 total species
+
+Dawson <- meta.data[,c(6:7, 21, 10, 20, 12, 19, 14, 11)]
+View(Dawson)
+str(Dawson)
+Dawson$Cultivar.Code <- as.factor(Dawson$Cultivar.Code)
+Dawson.species <- distinct(Dawson, Cultivar.Code, .keep_all = TRUE)
+View(Dawson.species)
+str(Dawson.species)
+Dawson.species <- Dawson.species[,5:8]
+View(Dawson.species)
+str(Dawson.species)
+spp.key <- read.csv("input/MasterSppKey_KatiePhilp490_28-1-2021.csv")
+str(spp.key)
+View(spp.key)
+str(spp.key)
+str(Dawson.species)
+Dawson.key <- Dawson.species %>% left_join(spp.key, by= c("Genus.Code","Species.Code", "Cultivar.Code"))
+View(Dawson.key)
+write.csv(Dawson.key, file="output/Dawson.key.csv")
+
+Master <- read.csv("output/MASTER.DAWSON.csv", na.strings="NA")
+View(Master)
+
+Dawson.Native <- left_join(Dawson, Master)
+View(Dawson.Native)
+str(Dawson.Native)
+Dawson.Native <- Dawson.Native %>% 
+  mutate(Native_SLL =as.factor(Native_SLL)) %>%
+  drop_na(Native_SLL)
+
+write.csv(Dawson.Native, file="output/Dawson.Trees.With.Origin.csv")
